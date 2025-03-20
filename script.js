0af94a8b8bf013dec813641ca2f7e45ed62e5ce1
TOMAS CABRAL RIOS <48458658@ort>
2025-03-20 12:04:02 -0300

    algo

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-function validacionTR()
+function validacionNombre()
 {
-    const nombre = document.getElementById("nombre").value
-    if(nombre.length <3)
+    const nombre = document.getElementById("Nombre").value
+    if(nombre.length > 3)
     {
 
     }
+}
+
+function validacionNombre()
+{
+    const Mail = document.getElementById("Email").value
+    if(String.Mail.includes("@gmail.com"))
+    {
+        
+    }
+}
+
+function validacionContraseña()
+{
+    const contra1 = document.getElementById("pass").value
+    const contra2 = document.getElementById("pass2").value
+    if(contra1 == contra2)
+    {
+
+    }    
+    
 }
